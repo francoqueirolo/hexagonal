7e82971c84bdd9f7c41ac58fb5656b875b3b87f9
jgomezz <jgomezz@gmail.com>
2025-09-17 20:58:14 -0500

    update

---
+++ src/main/java/com/tecsup/example/hexagonal/infrastructure/adapter/output/persistence/entity/UserEntity.java
@@ -0,0 +1,26 @@
+package com.tecsup.example.hexagonal.infrastructure.adapter.output.persistence.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(nullable = false, length = 150)
+    private String email;
+
+
+}
