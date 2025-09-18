aaff916c3394fafd875222f420e2b5d3dacea104
jgomezz <jgomezz@gmail.com>
2025-09-17 21:54:53 -0500

    update

--- src/main/java/com/tecsup/example/hexagonal/infrastructure/adapter/output/persistence/repository/UserRepositoryAdapter.java
+++ src/main/java/com/tecsup/example/hexagonal/infrastructure/adapter/output/persistence/repository/UserRepositoryAdapter.java
@@ -2,6 +2,7 @@ package com.tecsup.example.hexagonal.infrastructure.adapter.output.persistence.r
 
 import com.tecsup.example.hexagonal.application.port.output.UserRepository;
 import com.tecsup.example.hexagonal.domain.model.User;
+import com.tecsup.example.hexagonal.infrastructure.adapter.output.persistence.entity.UserEntity;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
@@ -11,6 +12,16 @@ public class UserRepositoryAdapter implements UserRepository {
 
     @Override
     public User save(User user) {
-        return null;
+
+        // Domain to Entity
+        UserEntity userEntity = null; // (UserEntity)user;
+
+        // Save entity
+        UserEntity entityCreated = this.jpaRepository.save(userEntity);
+
+        // Entity to Domain
+        User userCreated = null; //(User)entityCreated;
+
+        return userCreated;
     }
 }
