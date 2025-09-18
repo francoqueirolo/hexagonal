e275ebcd09958d9b3630ce9e8b337f39e082e493
jgomezz <jgomezz@gmail.com>
2025-09-17 22:56:21 -0500

    create UserController

---
+++ src/main/java/com/tecsup/example/hexagonal/infrastructure/adapter/input/rest/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.tecsup.example.hexagonal.infrastructure.adapter.input.rest.controller;
+
+
+import com.tecsup.example.hexagonal.application.port.input.UserService;
+import com.tecsup.example.hexagonal.domain.model.User;
+import com.tecsup.example.hexagonal.infrastructure.adapter.output.persistence.mapper.UserMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/users")
+@Slf4j
+public class UserController {
+
+    private final UserService userService;
+    private final UserMapper userMapper;
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        log.info("Creating user with name: {} and email: {}", user.getName(), user.getEmail());
+        return null;
+    }
+
+}
