7f3d92c7f432adc30dbdd65f222f54a28d979df2
jgomezz <jgomezz@gmail.com>
2025-09-19 19:57:25 -0500

    update

--- src/main/java/com/tecsup/example/hexagonal/infrastructure/adapter/input/rest/dto/UserResponse.java
+++ src/main/java/com/tecsup/example/hexagonal/infrastructure/adapter/input/rest/dto/UserResponse.java
@@ -1,4 +1,12 @@
 package com.tecsup.example.hexagonal.infrastructure.adapter.input.rest.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class UserResponse {
+    private Long id;
+    private String name;
+    private String email;
 }
