440bf43294d09efd0bbddedeb145238e8a02943a
jgomezz <jgomezz@gmail.com>
2025-09-29 21:12:40 -0500

    create SpringSecurity

---
+++ src/main/java/com/tecsup/example/hexagonal/infrastructure/adapter/input/security/SpringSecurity.java
@@ -0,0 +1,27 @@
+package com.tecsup.example.hexagonal.infrastructure.adapter.input.security;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SpringSecurity {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(authz -> authz
+                        .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/api/users/**").permitAll() //.authenticated()
+                        .anyRequest().authenticated()
+                );
+        return http.build();
+    }
+}
