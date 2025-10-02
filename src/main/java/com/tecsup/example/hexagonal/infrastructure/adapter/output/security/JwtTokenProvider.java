2ae3569b1c1aca7198c395e01180d25b21cb0ac0
jgomezz <jgomezz@gmail.com>
2025-10-01 21:40:17 -0500

    update JwtTokenProvider

--- src/main/java/com/tecsup/example/hexagonal/infrastructure/adapter/output/security/JwtTokenProvider.java
+++ src/main/java/com/tecsup/example/hexagonal/infrastructure/adapter/output/security/JwtTokenProvider.java
@@ -5,6 +5,7 @@ import com.tecsup.example.hexagonal.domain.model.User;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
@@ -15,14 +16,24 @@ import java.util.Date;
 @Component
 public class JwtTokenProvider {
 
+    @Value("${app.jwt.secret}")
+    private String jwtSecretString;
 
+    @Value("${app.jwt.expiration}")
+    private int jwtExpirationMs;
+
+    /**
+     * Generate token
+     * @param user
+     * @return
+     */
     public String generateToken(User user) {
         // Aquí iría la lógica para crear un token JWT real
 
         log.info("Creating token for email: {}", user.getEmail());
 
         Date now = new Date();
-        Date expiryDate = new Date(now.getTime() + 3600000); //
+        Date expiryDate = new Date(now.getTime() + jwtExpirationMs); //
 
         return Jwts.builder()
                 .subject(user.getEmail())  // .setSubject(user.getUsername())
@@ -40,7 +51,7 @@ public class JwtTokenProvider {
      * @return
      */
     private SecretKey getSigningKey() {
-        byte[] keyBytes = Base64.getDecoder().decode("MySecretWordMySecretWordMySecretWordMySecretWord");
+        byte[] keyBytes = Base64.getDecoder().decode(jwtSecretString);
         return Keys.hmacShaKeyFor(keyBytes);
     }
 
